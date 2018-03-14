6dcf9777a648dffa1ae88e3a4e42944771879216
arasu420 <36299277+arasu420@users.noreply.github.com>
2018-03-14 17:43:43 +0530

    Create mulofnum.cpp

---
+++ mulofnum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int a,i,c;
+    cin>>a;
+    for(i=1;i<=5;i++)
+    {
+    c=a*i;
+    cout<<c<<" ";
+    }        
+return 0;
+}
